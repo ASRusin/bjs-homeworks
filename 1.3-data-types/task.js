6ac07093f1b1f45d45d00aea18b06a50d8551846
Alexandr Rusin <Asr.i@mail.ru>
2020-10-07 22:53:54 +0300

    Homework 3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,64 @@
+"use strict";
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+  // код для задачи №1 писать здесь
+  if (Number.isFinite(percent)) {
+  } else {
+    console.log(
+      `Параметр "Процентная ставка" содержит неправильное значение ${percent}`
+    );
+  }
+
+  if (Number.isFinite(contribution)) {
+  } else {
+    console.log(
+      `Параметр "Начальный взнос" содержит неправильное значение ${contribution}`
+    );
+  }
+
+  if (Number.isFinite(amount)) {
+  } else {
+    console.log(
+      `Параметр "Общая стоимость" содержит неправильное значение ${amount}`
+    );
+  }
+
+  let amountOfCredit = amount - contribution;
+
+  let today = new Date();
+
+  let creditTerm =
+    (date.getYear() - today.getYear()) * 12 +
+    date.getMonth() -
+    today.getMonth();
+
+  percent /= 100;
+
+  let payment =
+    amountOfCredit *
+    (percent / 12 + percent / 12 / ((1 + percent / 12) ** creditTerm - 1));
+
+  let totalAmount = payment * creditTerm + contribution - contribution;
+
+  totalAmount = totalAmount.toFixed(2);
+
+  totalAmount = Number(totalAmount);
+
+  console.log(totalAmount);
+
+  return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+  // код для задачи №2 писать здесь
+  let greeting;
+
+  if (name) {
+    greeting = `Привет, мир! Меня зовут ${name}`;
+  } else {
+    greeting = "Привет, мир! Меня зовут Аноним";
+  }
+
+  console.log(greeting);
+
+  return greeting;
 }
