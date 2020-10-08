6065abbcc1a77ba834fe82f5aa002f7cd87aaf49
Alexandr Rusin <Asr.i@mail.ru>
2020-10-08 18:56:08 +0300

    Homework 3 - доработано

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,43 +1,27 @@
 "use strict";
 function calculateTotalMortgage(percent, contribution, amount, date) {
   // код для задачи №1 писать здесь
-  if (!Number.isFinite(percent) && Number.isNaN(percent)) {
-    console.log(
-      `Параметр "Процентная ставка" содержит неправильное значение ${percent}`
-    );
+  if (Number.isNaN(percent)) {
+    return `Параметр "Процентная ставка" содержит неправильное значение ${percent}`;
+  } else if (Number.isNaN(contribution)) {
+    return `Параметр "Начальный взнос" содержит неправильное значение ${contribution}`;
+  } else if (Number.isNaN(amount)) {
+    return `Параметр "Общая стоимость" содержит неправильное значение ${amount}`;
   }
 
-  if (!Number.isFinite(contribution) && Number.isNaN(contribution)) {
-    console.log(
-      `Параметр "Начальный взнос" содержит неправильное значение ${contribution}`
-    );
-  }
-
-  if (!Number.isFinite(amount) && Number.isNaN(amount)) {
-    console.log(
-      `Параметр "Общая стоимость" содержит неправильное значение ${amount}`
-    );
-  }
+  percent /= 100;
 
   let amountOfCredit = amount - contribution;
-
-  let today = new Date();
-
+  const today = new Date();
   let creditTerm =
     (date.getYear() - today.getYear()) * 12 +
     date.getMonth() -
     today.getMonth();
-
-  percent /= 100;
-
   let payment =
     amountOfCredit *
     (percent / 12 + percent / 12 / ((1 + percent / 12) ** creditTerm - 1));
-
   let totalAmount = payment * creditTerm + contribution - contribution;
-
   totalAmount = totalAmount.toFixed(2);
-
   totalAmount = Number(totalAmount);
 
   console.log(totalAmount);
@@ -47,15 +31,15 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
 
 function getGreeting(name) {
   // код для задачи №2 писать здесь
-  let greeting;
+  let greeting = `Привет, мир! Меня зовут ${name ? name : "Аноним"}`;
 
-  if (name) {
-    greeting = `Привет, мир! Меня зовут ${name}`;
-  } else {
-    greeting = "Привет, мир! Меня зовут Аноним";
-  }
+  // if (name) {
+  //   greeting = `Привет, мир! Меня зовут ${name}`;
+  // } else {
+  //   greeting = "Привет, мир! Меня зовут Аноним";
+  // }
 
-  console.log(greeting);
+  // console.log(greeting);
 
   return greeting;
 }
