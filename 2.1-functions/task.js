fd2f23a6d1123327d3946147117ac30eb4533070
Alexandr Rusin <Asr.i@mail.ru>
2020-10-11 13:42:47 +0300

    Решена задача 2

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -1,7 +1,6 @@
 "use strict";
 
 // Задание №1:
-
 function getSolutions(a,b,c) {
     let d = Math.pow(b, 2) - 4 * a * c;
     if (d < 0) {
@@ -29,6 +28,69 @@ function showSolutionsMessage(a,b,c) {
     };
 };
 
+// Проверки:
 showSolutionsMessage(1,2,3);
 showSolutionsMessage(7,20,-3);
 showSolutionsMessage(2,4,2);
+
+//Задача №2:
+function getAverageMark(marks) {
+    if (marks.length === 0) {
+        return 0;
+    };
+    let sum = 0;
+    for (let mark of marks) {
+        sum += mark;
+    };
+    return sum / (marks.length);
+};
+
+function getAverageScore(data) {    
+    let count = 0;
+    let average = 0;
+    if (Object.keys(data).length === 0){
+        data.average = 0;
+        return data;
+    };
+    for (let item in data) {
+        data[item] = getAverageMark(data[item]);
+        average += data[item];
+        count++
+    };
+    data.average = average / count;
+    return data;
+};
+
+//Проверки:
+console.log(getAverageScore({}));
+console.log(getAverageScore({
+    algebra: [2,4,5,2,3,4],
+    geometry: [2,4,5],
+    russian: [3,3,4,5],
+    physics: [5,5],
+    music: [2,2,6],
+    english: [4,4,3],
+    poetry: [5,3,4],
+    chemistry: [2],
+    french: [4,4]
+}));
+console.log(getAverageScore({
+    algebra: [2,4,5,2,3,4,2],
+    geometry: [2,4,5,4],
+    russian: [3,3,4,5,4,3],
+    physics: [5,5,2,2],
+    music: [2,2,3,5,4]    
+}));
+console.log(getAverageScore({
+    algebra: [2,4,5,2,3,4],
+    geometry: [2,4,5],
+    russian: [3,3,4,5],
+    physics: [5,5],
+    music: [2,2,6],
+    english: [4,4,3],
+    poetry: [5,3,4],
+    chemistry: [2],
+    french: [4,4],
+    psychology: [5,5,5,4,3,2,1],
+    informatics: [4,5,4,5,5]
+}));
