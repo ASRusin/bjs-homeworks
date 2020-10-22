1bffc71e61a6bed41cb7f056682f3df2d60862cd
Alexandr Rusin <Asr.i@mail.ru>
2020-10-22 21:26:47 +0300

    Реализованы 4 функции из задания 1

---
+++ 3.2-array-proto-hocs/functions.js
@@ -0,0 +1,20 @@
+console.clear();
+const weapons = [new Knife(), new Staff(), new Axe(), new StormStaff(), new LongBow(), new Bow()];
+function getNames(){
+    return weapons.map(item => item.name);
+};
+
+function getCountReliableWeapons(item){
+    return weapons.filter(weapon => weapon.durability > item).length;
+};
+
+function hasReliableWeapons(item){
+    return weapons.some(weapon => weapon.durability > item);
+};
+
+function getReliableWeaponsNames(item){
+    return weapons
+    .filter(weapon => weapon.durability > item)
+    .map(weapon => weapon.name);
+};
+
