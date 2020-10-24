2d3f3374cc04958c6713be2f17f7ed610916960d
Alexandr Rusin <Asr.i@mail.ru>
2020-10-24 15:24:22 +0300

    Задача 1 дополнена

--- 3.2-array-proto-hocs/functions.js
+++ 3.2-array-proto-hocs/functions.js
@@ -4,20 +4,32 @@ function getNames(){
     return weapons.map(item => item.name);
 };
 
-function getCountReliableWeapons(item){
+function getCountReliableWeapons(item) {
     return weapons.filter(weapon => weapon.durability > item).length;
 };
 
-function hasReliableWeapons(item){
+function hasReliableWeapons(item) {
     return weapons.some(weapon => weapon.durability > item);
 };
 
-function getReliableWeaponsNames(item){
+function getReliableWeaponsNames(item) {
     return weapons
     .filter(weapon => weapon.durability > item)
     .map(weapon => weapon.name);
 };
 
-function getTotalDamage(){
+function getTotalDamage() {
     return weapons.reduce((a, damage) => a += damage.attack, 0);
 };
+
+function getValuestCountToSumValues(numbers, sum) {
+    let count = 0;
+    numbers.reduce(function(a, num, i){
+        if (a < sum ){
+            count = i + 1;
+            return a += num;
+        };
+    } );
+    return count;
+};
+
